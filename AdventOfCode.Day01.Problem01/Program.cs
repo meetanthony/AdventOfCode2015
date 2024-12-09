89ca09f517aee31181d337de3278f28751a0f4b5
Anton Isakov <tonias@yandex.ru>
2024-12-09 18:50:37 +0300

    AdventOfCode.Day01.Problem01 solved

---
+++ AdventOfCode.Day01.Problem01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day01.Problem01;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var fileName = "day01.txt";
+        var lines = File.ReadAllLines(fileName);
+
+        var line = lines[0];
+        var upstairs = line.Count((c => c == '('));
+        var downstairs = line.Count((c => c == ')'));
+
+        Console.WriteLine(upstairs - downstairs);
+    }
+}
