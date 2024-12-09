11997d73a860e73a1cdefdda550b7e6a30d393bc
Anton Isakov <tonias@yandex.ru>
2024-12-09 18:50:55 +0300

    AdventOfCode.Day01.Problem02 solved

---
+++ AdventOfCode.Day01.Problem02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day01.Problem02;
+
+internal class Program
+{
+    private static void Main()
+    {
+        var fileName = "day01.txt";
+        var lines = File.ReadAllLines(fileName);
+
+        var floor = 0;
+        var charNumber = 0;
+        var line = lines[0];
+        while (floor != -1)
+        {
+            if (line[charNumber] == '(')
+                floor++;
+            else
+                floor--;
+            charNumber++;
+        }
+
+        Console.WriteLine(charNumber);
+    }
+}
