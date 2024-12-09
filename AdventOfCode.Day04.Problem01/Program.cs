0c9e14b2023b5e55e0d138a31758aabee3836065
Anton Isakov <tonias@yandex.ru>
2024-12-09 22:03:54 +0300

    AdventOfCode.Day04.Problem01 solved

---
+++ AdventOfCode.Day04.Problem01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode.Day04.Problem01;
+
+internal class Program
+{
+    private static void Main()
+    {
+        const int zerosCount = 5;
+        const string key = "ckczppom";
+        var ascii = new ASCIIEncoding();
+        
+        uint counter = 0;
+        while (true)
+        {
+            if (counter == uint.MaxValue)
+                throw new Exception("xyu");
+            var bytes = ascii.GetBytes(key + counter);
+            var hash = MD5.HashData(bytes);
+            var sb = new StringBuilder();
+            for (int i = 0; i < 3; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            if (sb.ToString().Contains("00000"))
+                break;
+
+            counter++;
+        }
+
+        Console.WriteLine(counter);
+    }
+}
