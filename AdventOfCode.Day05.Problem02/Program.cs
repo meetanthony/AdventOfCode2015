ce8c07166643a665ecde7fbff5dd8f5c535ff5f8
Anton Isakov <tonias@yandex.ru>
2024-12-10 16:31:08 +0300

    AdventOfCode.Day05.Problem02 solved

---
+++ AdventOfCode.Day05.Problem02/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode.Day05.Problem02;
+
+internal class Program
+{
+    private static void Main()
+    {
+        const string fileName = "day05.txt";
+        var lines = File.ReadAllLines(fileName);
+
+        var niceStringsCounter = 0;
+        var naughtyStrings = 0;
+
+        foreach (string line in lines)
+        {
+            var doubleLetterFounded = false;
+            var doubleLetter = "";
+            for (int i = 0; i < line.Length - 3; i++)
+            {
+                var substr = line.Substring(i, 2);
+                var lastIndex = line.LastIndexOf(substr);
+                if (lastIndex - i >= 2)
+                {
+                    doubleLetterFounded = true;
+                    doubleLetter = substr;
+                    break;
+                }
+            }
+
+            if (doubleLetterFounded == false)
+            {
+                naughtyStrings++;
+                goto NextStr;
+            }
+
+            var xyxFound = false;
+            var xyx = "";
+            for (int i = 0; i < line.Length - 2; i++)
+            {
+                if (line[i] == line[i + 2])
+                {
+                    xyx = line.Substring(i, 3);
+                    xyxFound = true;
+                    break;
+                }
+            }
+
+            if (xyxFound == false)
+            {
+                naughtyStrings++;
+                goto NextStr;
+            }
+
+            Console.WriteLine(line);
+
+            niceStringsCounter++;
+
+        NextStr:;
+        }
+
+        Console.WriteLine(niceStringsCounter);
+    }
+}
